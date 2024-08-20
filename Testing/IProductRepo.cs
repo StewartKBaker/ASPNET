23bf54b70d0ca93885bcaf7ba37a808738310971
StewartBaker <bakrstewart16@gmail.com>
2024-08-20 14:12:16 -0500

    finished project

---
+++ Testing/IProductRepo.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using Testing.Models;
+
+namespace Testing
+{
+    public interface IProductRepo
+    {
+        IEnumerable<Product> GetAllProducts();
+    }
+}
